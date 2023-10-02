c20e7226133aa3f1c92c00ca8d5c491f1217c740
Adrian Edwards <adrian@adriancedwards.com>
2023-10-02 14:24:59 -0400

    remove prints

--- icsmake.py
+++ icsmake.py
@@ -66,9 +66,6 @@ def parse_html(cachefilename="event.html", exclude_before=None, tz=""):
 		location = location.strip() + " - " + room
 		
 		e.location = location
-		print(e.begin)
-		print(starttime)
-		print(exclude_before)
 		if exclude_before and starttime > exclude_before:
 			c.events.add(e)
 		else:
